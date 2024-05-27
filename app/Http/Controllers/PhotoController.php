e911ca48265ceeeb0d7ef0287547238b12b90158
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-05-27 11:46:54 +0200

    Manual merge US 1

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -8,6 +8,42 @@ use Illuminate\Support\Facades\DB;
 
 class PhotoController extends Controller
 {
+    // Upload photo page
+    public function upload()
+    {
+        return view('photos.upload');
+    }
+
+    // Store photo page
+    public function store(Request $request)
+    {
+        // Validate submitted image
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // Try to upload image
+        try {
+            $imageName = time() . '.' . $request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+
+            // Save image to database
+            $product = new Photo();
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->image = 'images/' . $imageName;
+            $product->save();
+
+            // If succesfull upload, return to upload page with success message
+            return redirect()->route('photos.upload')->with('success', 'Photo uploaded successfully.');
+        } catch (\Exception $e) {
+            // If upload unsuccesfull, return to upload page with error
+            return redirect()->route('photos.upload')->with('error', 'Photo upload failed. Please try again.');
+        }
+    }
+
     public function index(Request $request, $info = null)
     {
         //* checks if the user has put in a search value
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,9 @@ Route::get('/photo', [PhotoController::class, 'index'])->name('photo.index');
 Route::post('/photo', [PhotoController::class, 'index'])->name('photo.index');
 
 Route::delete('/photo/delete/{photo}', [PhotoController::class, 'delete'])->name('photo.delete');
+
+// Photo upload form page
+Route::get('/photos/upload', [PhotoController::class, 'upload'])->name('photos.upload');
+
+// Form submit page
+Route::post('/photos/store', [PhotoController::class, 'store'])->name('photos.store');
