5955e75ac3f3c447012aff10c11b1b0746506c15
vincent van de merwe <335479@student.mboutrecht.nl>
2024-05-27 11:28:32 +0200

    added comments and added orderby's but also added DB to use the orderBy's

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,29 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PhotoController extends Controller
 {
     public function index(Request $request, $info = null)
     {
+        //* checks if the user has put in a search value
         if ($request != null) {
             $info = $request->input('search-value');
         }
+        //* if the info variable is empty then it shows all the records 
         if ($info == null) {
-            $photos = Photo::all();
+            $photos = DB::table('photos')->orderBy('id', 'desc')->get();
             return view('user_story_05.index', compact('photos'));
+            //* otherwise it checks the name of a photo matching the search value
         } else {
-            $photos = Photo::where('data', 'like', $info . '%')->get();
+            $photos = DB::table('photos')->where('data', 'like', $info . '%')->orderBy('id', 'desc')->get();
+            //* checks if the database doesn't give photos and then it will look if the search value matches the description
             if ($photos->isEmpty()) {
-                $photos = Photo::where('description', 'like', $info . '%')->get();
+                $photos = DB::table('photos')->where('description', 'like', $info . '%')->orderBy('id', 'desc')->get();
                 return view('user_story_05.index')->with(['photos' => $photos]);
             }
             return view('user_story_05.index')->with(['photos' => $photos]);
         }
     }
 
+    //* if this function gets called then it deletes the image that the user has selected to delete
     public function delete(Photo $photo)
     {
+        //* deletes the image
         $photo->delete();
         return redirect(route('photo.index'));
     }
