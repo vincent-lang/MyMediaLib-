7b29c86d842c6494a5b808df9a0d2b2af2952397
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-05-27 11:17:35 +0200

    Added comments

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -16,24 +16,29 @@ class PhotoController extends Controller
     // Store photo page
     public function store(Request $request)
     {
+        // Validate submitted image
         $request->validate([
             'name' => 'required',
             'description' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
+        // Try to upload image
         try {
             $imageName = time() . '.' . $request->image->extension();
             $request->image->move(public_path('images'), $imageName);
 
+            // Save image to database
             $product = new Photos();
             $product->name = $request->name;
             $product->description = $request->description;
             $product->image = 'images/' . $imageName;
             $product->save();
 
+            // If succesfull upload, return to upload page with success message
             return redirect()->route('photos.upload')->with('success', 'Photo uploaded successfully.');
         } catch (\Exception $e) {
+            // If upload unsuccesfull, return to upload page with error
             return redirect()->route('photos.upload')->with('error', 'Photo upload failed. Please try again.');
         }
     }
