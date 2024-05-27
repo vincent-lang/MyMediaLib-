c904565b17724c373c1f46b76bc8fca89f49d14d
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-05-27 11:15:24 +0200

    Added error message handling

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -21,16 +21,21 @@ class PhotoController extends Controller
             'description' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
-        $imageName = time() . '.' . $request->image->extension();
-        $request->image->move(public_path('images'), $imageName);
-        $product = new Photos();
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->image = 'images/' . $imageName;
 
-        $product->save();
-        
-        return redirect()->route('photos.upload')->with('success', 'Photo uploaded successfully.');
+        try {
+            $imageName = time() . '.' . $request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+
+            $product = new Photos();
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->image = 'images/' . $imageName;
+            $product->save();
+
+            return redirect()->route('photos.upload')->with('success', 'Photo uploaded successfully.');
+        } catch (\Exception $e) {
+            return redirect()->route('photos.upload')->with('error', 'Photo upload failed. Please try again.');
+        }
     }
 
     // Photo list page
